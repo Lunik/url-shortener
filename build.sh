fa8e0872e4826b745494b6ce75b177c21f9e1789
Lunik <lunik@tiwabbit.fr>
2023-01-22 17:57:25 +0100

    Dev GMZ
    Signed-off-by: Lunik <lunik@tiwabbit.fr>

--- build.sh
+++ build.sh
@@ -3,13 +3,15 @@
 rm -rf dist/
 mkdir -p dist/
 
-PYTHON_VERSION=3.10 # or 3.7, 3.8, ...
+PYTHON_IMAGE="python"
+PYTHON_VERSION=3.10-alpine # or 3.7, 3.8, ...
 docker run \
   --rm \
   -v $(pwd):/app \
   --workdir /app \
-  rg.fr-par.scw.cloud/scwfunctionsruntimes-public/python-dep:$PYTHON_VERSION \
-  pip install --upgrade pip && pip install \
+  --platform linux/amd64 \
+  $PYTHON_IMAGE:$PYTHON_VERSION \
+  pip3 install --upgrade pip && pip3 install \
     . \
     --target ./dist/package
 
